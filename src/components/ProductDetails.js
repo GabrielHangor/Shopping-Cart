d885fad9775f717e3ba7c0a14cf546fd43e1cfa3
HangorG <gaba93@yandex.ru>
2021-06-15 16:19:53 +0300

    product details styling

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -6,15 +6,19 @@ function ProductDetails({ addProduct }) {
 
   return (
     <div className="product-details-container">
-      <h1>{title}</h1>
-      <h2 style={{ textAlign: 'center' }}>{description}</h2>
-      <h3>${price}</h3>
-      <div className="image-wrapper">
-        <img src={image} alt={title} />
+      <div className="product-details-card-wrapper">
+        <div className="product-details-image-wrapper">
+          <img src={image} alt={title} />
+        </div>
+        <div className="details-info-wrapper">
+          <h1 style={{ textAlign: 'center' }}>{title}</h1>
+          <h2 style={{ textAlign: 'center' }}>{description}</h2>
+          <h3>${price}</h3>
+          <button onClick={() => addProduct(id)} className="add-btn">
+            ADD TO CART
+          </button>
+        </div>
       </div>
-      <button onClick={() => addProduct(id)} className="add-btn">
-        ADD TO CART
-      </button>
     </div>
   );
 }
